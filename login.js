c006cd873e4761cb519848199ae57e0bf24645f8
Kasey Lau <140150638+retoldminicraft@users.noreply.github.com>
2023-11-14 22:31:51 +0800

    Set up Sign in with Google

--- login.js
+++ login.js
@@ -1,12 +1,47 @@
-const loginButtonElement = document.querySelector('.login-button-js');
-const studentIdInputElement = document.querySelector('.studentid-input-js')
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
 
-loginButtonElement.addEventListener('click', () => {
-    const studentId = studentIdInputElement.value;
-    studentIdInputElement.value = ''
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
-    document.querySelector('.leaderboard').textContent = studentId;
-    // if (studentId.length = 7 & !isNaN(studentId)) {
+// Your web app's Firebase configuration
+import firebaseConfig from "./firebase-config.js";
 
-    // }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+const signInButton = document.querySelector('.sign-in-button-js');
+const signOutButton = document.querySelector('.sign-out-button-js');
+
+const userSignIn = async() => {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        const user = result.user;
+
+        const userName = user.displayName;
+        const userEmail = user.email;
+        console.log(user);
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    })
+}
+
+const userSignOut = async() => {
+    signOut(auth).then(() => {
+        alert("You have signed out successfully!")
+    }).catch((error) => {})
+}
+
+onAuthStateChanged(auth, (user) => {
+    if(user) {
+        alert("You have signed in!")
+    } else {
+        // Signed out
+    }
 })
+
+signInButton.addEventListener('click', userSignIn);
+signOutButton.addEventListener('click', userSignOut);
